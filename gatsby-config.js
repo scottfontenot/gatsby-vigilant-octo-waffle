ee2c43a6b5eed63959854a1d1e0284e0405cfd9f
Scott Fontenot <fontenot.scott@gmail.com>
2018-06-04 05:52:47 -0400

    #7-added image sharp plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,18 @@ module.exports = {
     title: 'Scott Fontenot',
     desc: 'My coding journey',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    'gatsby-plugin-styled-components',
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "img",
+        path: `${__dirname}/src/images`
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
+  
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
+import Img from 'gatsby-image';
 
 const IndexPage = ({data}) => (
   <div>
     <h1>Hello people</h1>
+    <Img sizes={data.background.sizes} />
     <p>{data.site.siteMetadata.title}</p>
     <p>{data.site.siteMetadata.desc}</p>
     <p>Welcome to your new Gatsby site.</p>
@@ -22,6 +24,11 @@ query SiteMeta {
       desc
     }
   }
+  background: imageSharp(id: {regex: "/bg.jpeg/"}) {
+   sizes(maxWidth: 1240) {
+     ...GatsbyImageSharpSizes
+   }
+ }
 }
 `;
 
