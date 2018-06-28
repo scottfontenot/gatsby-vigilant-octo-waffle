69d8f3088890d2a180dd6ab20f2741a4afbbc610
Scott Fontenot <fontenot.scott@gmail.com>
2018-06-27 22:16:16 -0400

    updated about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,8 +5,9 @@ export default class About extends Component {
     return (
       <div>
         <h1>About Me</h1>
-        <p>whatever Banksy blog kitsch sustainable post-ironic synth fashion axe iPhone gastropub Carles mumblecore polaroid letterpress  actually viral paleo fanny pack Echo Park bicycle rights Thundercats literally PBR&B Cosby sweater tousled cardigan 90's normcore flexitarian distillery hella banh mi Etsy mlkshk fap Vice yr cray seitan sriracha XOXO raw denim trust fund freegan lomo chambray swag keffiyeh forage 3 wolf moon Odd Future lo-fi small batch heirloom craft beer typewriter bespoke meggings Tumblr pork belly directKickstarter trade  disrupt farm-to-table butcher plaid vinyl ethical put a bird on it before they sold out gluten-free quinoa skateboard tofu umami tattooed twee you probably haven't heard of them photo booth locavore next level bitters deep v four loko beard Helvetica biodiesel aesthetic semiotics mixtape kale chips fingerstache brunch roof party Blue Bottle Godard Neutra cornhole occupy ennui retro authentic readymade keytar pour-over kogi Austin dreamcatcher stumptown hoodie gentrify drinking vinegar art party slow-carb shabby chic banjo wolf fixie tote bag church-key flannel meh wayfarers High Life irony street art Pitchfork selvage pop-up Marfa mustache McSweeney's YOLO salvia master cleanse pug Pinterest food truck Portland jean shorts leggings sartorial Schlitz VHS organic vegan selfies narwhal +1 hashtag single-origin coffee Shoreditch Bushwick Intelligentsia Williamsburg messenger bag Truffaut chillwave cred cliche Wes Anderson scenester 8-bit Tonx Brooklyn crucifix American Apparel DIY squid ugh PBR pickled chia try-hard artisan asymmetrical
-        </p>
+        <p>I’m Scott Fontenot, a late bloomer to web development living in Washington DC. Yep, I work in the Federal 
+        Government, doing a little front-end development, a little back-end, and mixing in some data visualization. This blog is about JavaScript, 
+        Python, Qliksense, but mostly about my journey, the struggles, the experiences, etc. </p>
       </div>
     )
   }
